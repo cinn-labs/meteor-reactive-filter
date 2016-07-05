1f783b239622c0052e585d8d42044cf264190932
Christian Storch <ccstorch@gmail.com>
2016-07-05 14:45:50 +0100

    0.0.2

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'cinn:reactive-filter',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Reactive filter helper for Meteor + React apps',
   git: 'https://github.com/cinn-labs/meteor-reactive-filter',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.3.1');
+  api.versionsFrom('1.3.4');
   api.use('ecmascript');
   api.use('tracker');
-  api.use('cinn:react-infinite-scroll@0.0.1');
-  api.use('cinn:react-form-helpers@1.0.1');
+  api.use('cinn:react-infinite-scroll@0.0.2');
+  api.use('cinn:react-form-helpers@1.1.2');
   api.use('ecmascript');
 
   api.export('Filter');
